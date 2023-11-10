85dccadc9c2245469cc6c0e7abcfed960c4cb7a5
jWinman91 <jwinkelm@tcd.ie>
2023-11-10 22:17:52 +0100

    fixed bug with None

--- utils/text_editor.py
+++ utils/text_editor.py
@@ -21,7 +21,7 @@ class Editor:
             module_name = model["model_wrapper"].split("/")[0]
             model_name = model["model_wrapper"].split("/")[1]
             param_filename = model.get("model_config", None)
-            if param_filename.endswith(".yaml"):
+            if param_filename and param_filename.endswith(".yaml"):
                 params = self.load_yml(param_filename) if param_filename else {}
             else:
                 params = param_filename
