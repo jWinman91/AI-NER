97b071d1f4e9cc13b09d1e3beb66c982ee847b99
jWinman91 <jwinkelm@tcd.ie>
2024-03-19 17:03:47 +0100

    using docker in startup now

--- startup.sh
+++ startup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-docker network create JensNetwork
+if ! docker network ls --format '{{.Name}}' | grep -wq "JensNetwork"; then
+  docker network create JensNetwork
+fi
+
 if [ "$(docker ps -a -q -f name=config_db)" ]; then
   if [ ! "$(docker ps -q -f name=config_db)" ]; then
     docker start "$(docker ps -a -q -f name=config_db)"
@@ -11,19 +14,9 @@ else
   sleep 5
 fi
 
-docker build --tag ai_ner .
-docker run --network=JensNetwork -p 8000:8000 --name AINER ai_ner
-
-#if ps aux | grep -v grep | grep "python app.py" > /dev/null; then
-#  echo "app.py is already running!"
-#else
-#  echo "Starting app.py ..."
-#  source venv/bin/activate
-#  python app.py > logs.txt  2>&1 &
-#  background_pid=$!
-#  if kill -0 "$background_pid" 2>/dev/null; then
-#    echo "app.py is running!"
-#  else
-#    echo "Something went wrong..."
-#  fi
-#fi
+if [ ! "$(docker ps -q -f name=AINER)" ]; then
+  docker build --tag ai_ner .
+  docker run --network=JensNetwork -p 8000:8000 --name AINER ai_ner
+else
+  echo "Container is already running!"
+fi
